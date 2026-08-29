cfb74604c163f7e9a02e3bda7302c1622d1386cc
Manikantha <manikantha.mkn@gmail.com>
2026-08-29 16:22:52 +0530

    hello world started

--- 07.array.sh
+++ 07.array.sh
@@ -1,8 +1,10 @@
 #!/bi/bash
 
-MOVIES=("Fast&Furious" "Hitman" "Spiderman" "Suits" "Sniper" "Iron Man")
+MOVIES=("Fast&Furious" "Hitman" "Spiderman" "Suits" "Sniper" "Iron Man" "I'm just trying to push the code from VS only")
 
 echo "First Movie: ${MOVIES[0]}"
 echo "First Movie: ${MOVIES[3]}"
 
 echo "All Movies: ${MOVIES[@]}"
+
+#I'm just trying to push the code from VS only
