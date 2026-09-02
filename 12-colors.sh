371bfcf4403691248479bda82450378102a4c63d
manikantha <manikantha.mkn@gmail.com>
2026-09-02 13:24:37 +0530

    Shell Practice

--- 12-colors.sh
+++ 12-colors.sh
@@ -3,3 +3,12 @@
 echo -e "\e[31m Hello colors \e[0m"
 
 echo "Hello no Colors"
+echo -e "Hello World"
+echo -e "\e[31m Manikantha"
+echo -e "\e[32m Manikantha"
+echo -e "\e[33m Manikantha"
+echo -e "\e[34m Manikantha"
+echo -e "\e[35m Manikantha"
+echo -e "\e[36m Manikantha"
+echo -e "\e[37m Manikantha"
+echo -e "\e[0m Manikantha"
