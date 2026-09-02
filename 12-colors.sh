b2a48f79031739a4c18098b539a45c1fc90d021d
manikantha <manikantha.mkn@gmail.com>
2026-09-02 13:38:56 +0530

    Shell Practice

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 echo -e "\e[31m Hello colors"
-
-echo "Hello no Colors \e[0m"
 echo -e "Hello World"
-echo -e "\e[31m Manikantha"
+echo "Once the color is enabled, it will continue to affect the following commands until we reset the color back to normal."
+
+echo -e "\e[31m Manikantha \e[0m"
 echo -e "\e[32m Manikantha"
 echo -e "\e[33m Manikantha"
 echo -e "\e[34m Manikantha"
@@ -12,3 +12,4 @@ echo -e "\e[35m Manikantha"
 echo -e "\e[36m Manikantha"
 echo -e "\e[37m Manikantha"
 echo -e "\e[0m Manikantha"
+echo -e "\e[30m Manikantha"
