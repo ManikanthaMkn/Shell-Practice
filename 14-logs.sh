2f87f495840096d637bf367440fc57e5f1c95101
manikantha <manikantha.mkn@gmail.com>
2026-09-02 16:14:28 +0530

    Shell Practice

--- 14-logs.sh
+++ 14-logs.sh
@@ -15,18 +15,18 @@ echo "Script Started Executing at:: $(date)" &>>$LOG_FILES
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N" &>>$LOG_FILES
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILES
     exit 1
 else
-    echo "You are running with root access" &>>$LOG_FILES
+    echo "You are running with root access" | tee -a $LOG_FILES
 fi #IF I am not root → show error and stop. Otherwise → continue.
 
 VALIDATE(){
     if [ "$1" -eq 0 ]
     then 
-        echo -e "Installing $2 is ... $G SUCCESS $N" &>>$LOG_FILES
+        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILES
     else 
-        echo -e "Installing $2 is ... $R FAILURE $N" &>>$LOG_FILES
+        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILES
         exit 1
     fi
 }
@@ -35,39 +35,39 @@ dnf list installed mysql &>>$LOG_FILES
 
 if [ $? -ne 0 ] #$? The exit status of the most recently executed command
 then
-    echo -e "$R MySQL is not installed $N ... $G going to install it $N" &>>$LOG_FILES
+    echo -e "$R MySQL is not installed $N ... $G going to install it $N" &| tee -a $LOG_FILES
     
     dnf install mysql -y &>>$LOG_FILES
 
     VALIDATE "$?" "MySQL"
 else
-    echo -e "$Y MySQL is already installed $N ... Nothing to do" &>>$LOG_FILES
+    echo -e "$Y MySQL is already installed $N ... Nothing to do" | tee -a $LOG_FILES
     # exit 0
 fi
 
 dnf list installed nginx &>>$LOG_FILES
 if [ $? -ne 0 ] #$? The exit status of the most recently executed command
 then
-    echo -e "$R nginx is not installed $N ... $G going to install it $N" &>>$LOG_FILES
+    echo -e "$R nginx is not installed $N ... $G going to install it $N" | tee -a $LOG_FILES
     
     dnf install nginx -y &>>$LOG_FILES
 
     VALIDATE "$?" "ngnix"
 else
-    echo -e "$Y nginx is already installed $N ... Nothing to do" &>>$LOG_FILES
+    echo -e "$Y nginx is already installed $N ... Nothing to do" | tee -a $LOG_FILES
     # exit 0
 fi
 
 dnf list installed python3 &>>$LOG_FILES
 if [ $? -ne 0 ] #$? The exit status of the most recently executed command
 then
-    echo -e "$R python3 is not installed $N ... $G going to install it $N" &>>$LOG_FILES
+    echo -e "$R python3 is not installed $N ... $G going to install it $N" | tee -a $LOG_FILES
     
     dnf install python3 -y &>>$LOG_FILES
 
     VALIDATE "$?" "python3"
 else
-    echo -e "$Y python3 is already installed $N ... Nothing to do" &>>$LOG_FILES
+    echo -e "$Y python3 is already installed $N ... Nothing to do" | tee -a $LOG_FILES
     # exit 0
 fi
 #Manikantha
