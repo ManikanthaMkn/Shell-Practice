d2331d2ef8132373c5f55c09ab9beda9c7e6dcdd
manikantha <manikantha.mkn@gmail.com>
2026-09-02 16:08:44 +0530

    Shell Practice

---
+++ 14-logs.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILES="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script Started Executing at:: $(date)" &>>$LOG_FILES
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" &>>$LOG_FILES
+    exit 1
+else
+    echo "You are running with root access" &>>$LOG_FILES
+fi #IF I am not root → show error and stop. Otherwise → continue.
+
+VALIDATE(){
+    if [ "$1" -eq 0 ]
+    then 
+        echo -e "Installing $2 is ... $G SUCCESS $N" &>>$LOG_FILES
+    else 
+        echo -e "Installing $2 is ... $R FAILURE $N" &>>$LOG_FILES
+        exit 1
+    fi
+}
+
+dnf list installed mysql &>>$LOG_FILES
+
+if [ $? -ne 0 ] #$? The exit status of the most recently executed command
+then
+    echo -e "$R MySQL is not installed $N ... $G going to install it $N" &>>$LOG_FILES
+    
+    dnf install mysql -y &>>$LOG_FILES
+
+    VALIDATE "$?" "MySQL"
+else
+    echo -e "$Y MySQL is already installed $N ... Nothing to do" &>>$LOG_FILES
+    # exit 0
+fi
+
+dnf list installed nginx &>>$LOG_FILES
+if [ $? -ne 0 ] #$? The exit status of the most recently executed command
+then
+    echo -e "$R nginx is not installed $N ... $G going to install it $N" &>>$LOG_FILES
+    
+    dnf install nginx -y &>>$LOG_FILES
+
+    VALIDATE "$?" "ngnix"
+else
+    echo -e "$Y nginx is already installed $N ... Nothing to do" &>>$LOG_FILES
+    # exit 0
+fi
+
+dnf list installed python3 &>>$LOG_FILES
+if [ $? -ne 0 ] #$? The exit status of the most recently executed command
+then
+    echo -e "$R python3 is not installed $N ... $G going to install it $N" &>>$LOG_FILES
+    
+    dnf install python3 -y &>>$LOG_FILES
+
+    VALIDATE "$?" "python3"
+else
+    echo -e "$Y python3 is already installed $N ... Nothing to do" &>>$LOG_FILES
+    # exit 0
+fi
+#Manikantha
